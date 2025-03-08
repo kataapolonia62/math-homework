b357ba9de8c56989924202d9aea8dbc48146b68a
kataapolonia62 <kataapolonia62@gmail.com>
2025-03-08 16:12:02 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MathHomework {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = input.nextInt();
+    System.out.println("The square root of " + num + " is " + Math.sqrt(num));
+  }
+}
