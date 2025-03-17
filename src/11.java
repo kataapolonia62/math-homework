81b97f45c364277f034430b25c0d54fcc89db988
kataapolonia62 <kataapolonia62@gmail.com>
2025-03-17 12:13:29 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class MathHomework {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int num1 = rand.nextInt(10);
+        int num2 = rand.nextInt(10);
+        System.out.println("What is " + num1 + " x " + num2 + "?");
+    }
+}
