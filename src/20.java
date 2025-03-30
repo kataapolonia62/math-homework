2777f55034cffe2dfebc9b24ca979b9edd81de40
kataapolonia62 <kataapolonia62@gmail.com>
2025-03-30 21:27:38 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        double c = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + c);
+    }
+}
