473907550d4e1e68a87bb68a7819556e7620816f
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-01 18:29:36 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of performing basic arithmetic operations and printing the result
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum is: " + (num1 + num2));
+        System.out.println("The difference is: " + (num1 - num2));
+        System.out.println("The product is: " + (num1 * num2));
+        System.out.println("The quotient is: " + (num1 / num2));
+    }
+}
