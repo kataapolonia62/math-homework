53172e3b665286973a6cd93335899e4d5567da38
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-03 13:25:28 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,18 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of arithmetic operations in Java
+        int result = 5 + 3;
+        System.out.println("The sum is: " + result);
+
+        // Another example using loop and variables
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        // Example of input/output operations in Java
+        String userInput = "Hello, World!";
+        System.out.println("User input: " + userInput);
+    }
+}
