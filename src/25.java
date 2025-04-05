2ff3265fb67bcf6ac6a6263dabb5d8fddfb55a7b
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-05 06:44:51 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for mathematical operations and calculations
+        System.out.println("Calculating 2 + 3 = " + (2 + 3));
+        System.out.println("Square root of 16 is: " + Math.sqrt(16));
+        System.out.println("Difference between 9 and 4 is: " + (9 - 4));
+    }
+}
