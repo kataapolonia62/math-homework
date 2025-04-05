f86756e831000fb7c7d6d6dce4127b3a944510d5
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-05 06:22:02 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a basic Java program
+        int x = 5;
+        System.out.println("The value of x is " + x);
+    }
+}
