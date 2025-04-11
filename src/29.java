b5fb793f8ecbda033f491f24ab93ec9cddfef052
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-11 10:43:59 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        double result = (num1 + num2) / 2;
+        System.out.println("The average of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
