f5785bb88784f5f11dc47e0ea19ef3c05ce153db
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-24 21:06:58 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.2;
+        String result = "The sum of x and y is: " + (x + y);
+        System.out.println(result);
+    }
+}
