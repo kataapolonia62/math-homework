3fb20e9de3c111505294df6fe96f5c3eb6c162bf
kataapolonia62 <kataapolonia62@gmail.com>
2025-04-27 02:10:21 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of performing simple arithmetic operations
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+
+        float product = 4.0f * 2.5f;
+        System.out.println("The product is: " + product);
+    }
+}
