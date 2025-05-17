2c6235d45378015854e0a35022cc21468a23f672
kataapolonia62 <kataapolonia62@gmail.com>
2025-05-17 04:09:21 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
