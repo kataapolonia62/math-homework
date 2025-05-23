c44aebaaef5a3e5d3815d6761b4a49a15104c0b6
kataapolonia62 <kataapolonia62@gmail.com>
2025-05-23 04:49:26 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,14 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical operation
+        double result = 5.0 + 3.0;
+        System.out.println("The sum is: " + result);
+        
+        // Example of conditional statement
+        if (true) {
+            System.out.println("This line will be printed");
+        } else {
+            System.out.println("This line will not be printed");
+        }
+    }
+}
