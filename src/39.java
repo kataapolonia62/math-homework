e0c01d4be97c3bbe09f5ee1a83b199dad5cb0fe6
kataapolonia62 <kataapolonia62@gmail.com>
2025-05-25 21:36:26 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a simple arithmetic problem
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+        
+        // Another example using loops and conditional statements
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is an even number");
+            } else {
+                System.out.println(i + " is an odd number");
+            }
+        }
+    }
+}
