e64b11e9f549dfeb8c8b445389404591b1a1e414
kataapolonia62 <kataapolonia62@gmail.com>
2025-05-27 17:59:38 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Random number generation
+        int randomNumber = (int)(Math.random() * 100);
+        System.out.println("Random Number: " + randomNumber);
+
+        // Example calculation
+        double sum = 5.0 + 3.0;
+        System.out.println("Sum of 5 and 3: " + sum);
+
+        // Example arithmetic operation
+        int difference = 7 - 2;
+        System.out.println("Difference between 7 and 2 is: " + difference);
+    }
+}
